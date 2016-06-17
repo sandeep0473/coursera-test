29051b74f518d1b4a3be25985e4b09bb6d9f2c4b
siva sandeep garapati <sandeepgarapati21@gmail.com>
2016-06-17 12:52:02 -0400

    file edited

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+( function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -57,10 +57,10 @@ for (var i=0; i<names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === 'j') {
+  if (firstLetter=== 'j') {
      byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
   }
 }
-)();
+})();
